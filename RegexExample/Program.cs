480c3723bac1ca5abca318fe273702287e8ca0e2
konikasingh <konikasingh1996@gmail.com>
2021-10-31 14:38:38 +0530

    Validating Mobile Number with country code

--- RegexExample/Program.cs
+++ RegexExample/Program.cs
@@ -7,11 +7,16 @@ namespace RegexExample
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Regex Example");
-            RegexValidation rege = new RegexValidation();
+            RegexValidation rege = new RegexValidation(); //object creation of class
+
             bool ValidResult = rege.ValidateFirstName();
-            Console.WriteLine("Name"+" "+ValidResult);
+            Console.WriteLine("Name"+" "+ValidResult); //print the valid result of first name
+
             bool ValidResu = rege.ValidateLasttName();
-            Console.WriteLine("Name" + " " + ValidResu);
+            Console.WriteLine("Name" + " " + ValidResu); //print the valid result of last name
+
+            bool ValidMobileResult = rege.ValidMobileNumber();
+            Console.WriteLine("Mobile" + " " + ValidMobileResult); //print the valid mobile number with country code:
         }
     }
 }
--- RegexExample/UC2RegexValidation.cs
+++ RegexExample/UC2RegexValidation.cs
@@ -10,15 +10,26 @@ namespace RegexExample
     public class RegexValidation
     {
         public static string NAME_REGEX = "^[A-Z]{1}[A-Za-z]{3,}$";
+        public static string MOBILENUMBER_REGEX = "^(?:[0-9]{2}[ ][0-9]{10})$";
 
+        //Validating the First name with first word capital and other are small:
         public bool ValidateFirstName()
         {
             return Regex.IsMatch("Konika", NAME_REGEX);
            
         }
+
+        //Validating the last name with first word capital and other are small:
         public bool ValidateLasttName()
         {
             return Regex.IsMatch("Singh", NAME_REGEX);
+
+        }
+
+        //Validating the last mobile number in which country code and space is followed and then the 10 digit mobile number:
+        public bool ValidMobileNumber()
+        {
+            return Regex.IsMatch("91 8766567855", MOBILENUMBER_REGEX);
 
         }
     }
