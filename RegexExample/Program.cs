213d6018d61591375fed230a17c615eef4f3a381
konikasingh <konikasingh1996@gmail.com>
2021-10-31 20:57:30 +0530

    Validating Email Address with Case sensative

--- RegexExample/Program.cs
+++ RegexExample/Program.cs
@@ -10,13 +10,17 @@ namespace RegexExample
             RegexValidation rege = new RegexValidation(); //object creation of class
 
             bool ValidResult = rege.ValidateFirstName();
-            Console.WriteLine("Name"+" "+ValidResult); //print the valid result of first name
+            Console.WriteLine("Name: "+" "+ValidResult); //print the valid result of first name
 
             bool ValidResu = rege.ValidateLasttName();
-            Console.WriteLine("Name" + " " + ValidResu); //print the valid result of last name
+            Console.WriteLine("Name: " + " " + ValidResu); //print the valid result of last name
 
             bool ValidMobileResult = rege.ValidMobileNumber();
-            Console.WriteLine("Mobile" + " " + ValidMobileResult); //print the valid mobile number with country code:
+            Console.WriteLine("Mobile Number: " + " " + ValidMobileResult); //print the valid mobile number with country code:
+
+            bool ValidEmail = rege.ValidEmail();
+            Console.WriteLine("Email Address: " + " " + ValidEmail);
+           // Console.WriteLine("Mobile" + " " + ValidEmail);
         }
     }
 }
